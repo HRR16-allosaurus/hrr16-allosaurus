06ebb20b58db4285c25292d6a90e01bebb80f505
Chris Weaver <cmagweaver@gmail.com>
2016-05-27 11:53:14 -0700

    Added post request to routing

--- server/db/routing.js
+++ server/db/routing.js
@@ -1,19 +1,19 @@
 //main setup for routing mongoose to server
-
+var handler = require('./controller');
 //export modules
 module.exports = function(app){
   
     console.log('This is the routing module punk!');
   //setup routing 
-
-
+  // controller.createTrip();
   //get requests
   
   
   //post requests
-  
-  
-  
+    // summary
+  app.post('/summary',function(request, response){
+    handler.createTrip(request,response);
+  });
   
   
 };
