d42e4161f5bb9fe7d37ef88a649dc0a1c18bcb27
ericdsmith <tech.ericsmith@gmail.com>
2016-05-27 14:49:55 -0400

    createTrip and getTrips functions created

--- server/db/controller.js
+++ server/db/controller.js
@@ -1,8 +1,27 @@
 //controller for db access
 //parent of model and connections
-var trip = require('TripPlanner');
+var trip = require('./model.js');
 //function handlers for db (e.g store, etc.) on trip 
 
+exports.createTrip = function(req, res){
+  console.log("I've been called");
+  new trip({TripName: req.body.TripName,
+    Where: req.body.Where,
+    When: req.body.When,
+    Gear: req.body.Gear
+  }).save();
+
+};
+
+exports.getTrips = function(req, res){
+  trip.find({}, function(err, trips){
+    if(err){
+      console.log(err);
+    } else {
+      res.json(trips);
+    }
+  })
+};
 
 
-//export module
+//export moduleasdf
