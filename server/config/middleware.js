50d598be5175a3d615a5195290f0cf25dc243074
Chris Weaver <cmagweaver@gmail.com>
2016-05-26 20:38:56 -0700

    Added config folder/file DB:added controller, routing

---
+++ server/config/middleware.js
@@ -0,0 +1,12 @@
+//import modules
+
+
+module.exports = function(app, express){
+  console.log('You are now in middleware punk!');
+//basic configuration for express
+  // set viewing engine
+  // set encoding 
+  // static file directory 
+
+//export module
+};
