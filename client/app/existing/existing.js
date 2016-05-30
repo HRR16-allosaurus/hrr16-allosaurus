c52ff9b018776232a5900056a6a7dfd40a2a4ba7
Timothy Woo <timwoo24@gmail.com>
2016-05-30 13:00:34 -0700

    update front end - existing trips feature

--- client/app/existing/existing.js
+++ client/app/existing/existing.js
@@ -3,32 +3,22 @@ angular.module('hikeplanner.existing', [])
 .controller('existingController', function($scope, $location, $http) {
 
   $scope.allTrips = [];
+  $scope.tripNames = [];
 
   $scope.getAll = function() {
     console.log('retrieved');
     // return $http({
     //   method: 'GET',
-    //   url: '/summary',
-    //   data: $scope.tripData
+    //   url: '/summary'
     // })
     // .then(function (resp) {
-    //   $scope.allTrips = resp.
+    //   $scope.allTrips = resp;
     // });
     $scope.allTrips = data;
+    $scope.tripNames = _.pluck($scope.allTrips, "name");
     console.log($scope.allTrips);
   };
-  
 
 });
 
 
-// MOCK DATA
-var data = [
-  {
-    name: 'birthday',
-    where: 'yosemite',
-    begin: '2016-05-27',
-    end: '2016-05-31',
-    supplies: ['water', 'food', 'other', 'test']
-  }
-]
---
+++ client/data.js
@@ -0,0 +1,17 @@
+// MOCK DATA
+var data = [
+  {
+    name: 'birthday',
+    where: 'yosemite',
+    begin: '2016-01-04',
+    end: '2016-01-07',
+    supplies: ['water', 'food', 'other', 'test']
+  },
+  {
+    name: 'memorial day',
+    where: 'zion',
+    begin: '2016-05-28',
+    end: '2016-05-31',
+    supplies: ['water', 'food', 'other', 'test']
+  }
+]
