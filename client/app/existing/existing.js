be6c5a91b8d2e470efae5233d4df00eb48f74176
Timothy Woo <timwoo24@gmail.com>
2016-06-06 15:56:49 -0700

    refactor chat to broadcast to everyone except sender; refactor code to create Users Factory to be used by both existingController and newTripController

--- client/app/existing/existing.js
+++ client/app/existing/existing.js
@@ -1,10 +1,13 @@
 angular.module('hikeplanner.existing', [])
 
-.controller('existingController', function($rootScope, $scope, $location, $http) {
+.controller('existingController', function($rootScope, $scope, $location, $http, Users) {
 
   $scope.allTrips = [];
   $scope.tripNames = [];
+  $scope.users = Users.users;
+  $scope.usersHash = Users.usersHash;
   
+  // get all trips that user created as well as trips user has been invited to
   $http({
       method: 'GET',
       url: '/summary' + '/' + $rootScope.profile.user_id
